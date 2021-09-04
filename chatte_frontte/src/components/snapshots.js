197b74bc133fe36d8bad0d1244b6604116aebae6
secolema <secolema@iu.edu>
2021-09-04 15:03:43 -0500

    snapshot

--- chatte_frontte/src/components/snapshots.js
+++ chatte_frontte/src/components/snapshots.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import styled from 'styled-components';
+
+
+const Styledcontainer = styled.div`
+
+    width: 100%;
+    height: 400px;
+    background-color: white;
+
+`;
 
 function Snapshots() {
     return (
-        <div>
+        <Styledcontainer>
             
-            
-        </div>
+            <h1>snapshots</h1>
+        </Styledcontainer>
     )
 } 
 
