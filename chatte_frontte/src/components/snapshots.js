65142d0c395799abc6419aa84b372d83d3bceb8a
secolema <secolema@iu.edu>
2021-09-04 15:46:32 -0500

    snapshot updates

--- chatte_frontte/src/components/snapshots.js
+++ chatte_frontte/src/components/snapshots.js
@@ -3,18 +3,48 @@ import styled from 'styled-components';
 
 
 const Styledcontainer = styled.div`
+    margin-top: 30px; 
 
     width: 100%;
-    height: 400px;
-    background-color: white;
+    height: 340px;
+    background-color: #6085A6; 
+
+    display: flex;
+    justify-content: center;
 
 `;
+
+
+const Styledboxes = styled.div`
+    width: 17%;
+    height: 80%;
+    background-color: rgba(230,230,230,.75);
+    margin-top: 25px;
+    border-radius: 10%;
+
+    margin-left: 10px;
+    margin-right: 10px;
+`;
+
 
 function Snapshots() {
     return (
         <Styledcontainer>
-            
-            <h1>snapshots</h1>
+            <Styledboxes>
+            </Styledboxes>
+
+            <Styledboxes>
+            </Styledboxes>
+
+            <Styledboxes>
+            </Styledboxes>
+
+            <Styledboxes>
+            </Styledboxes>
+
+            <Styledboxes>
+            </Styledboxes>
+        
         </Styledcontainer>
     )
 } 
