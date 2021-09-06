c94da69c4897fdbbe49b35801676584fcfac45a1
secolema <secolema@iu.edu>
2021-09-06 10:35:47 -0500

    snapshots

---
+++ chatte_frontte/src/components/Snapshots.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import React from 'react';
+
+
+function Snapshots() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Snapshots
